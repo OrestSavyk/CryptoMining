f6def534c148a7de0c41a3649d21fa0e58fb4dc6
Orest Savyk <orestsavyk@MacBook-Pro-von-Orest.local>
2022-01-11 17:38:43 +0200

    Implement validation errors

--- src/app/components/content/input-errors/input-errors.component.ts
+++ src/app/components/content/input-errors/input-errors.component.ts
@@ -1,14 +1,29 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { FieldErrors } from './error-maping.const';
 
 @Component({
   selector: 'app-input-errors',
   templateUrl: './input-errors.component.html',
   styleUrls: ['./input-errors.component.scss'],
 })
-export class InputErrorsComponent implements OnInit {
-  @Input()
-  showErrors: boolean;
-  constructor() {}
+export class InputErrorsComponent implements OnChanges {
+  @Input() showErrors: boolean;
+  @Input() error: any;
+  @Input() errorMapingKey: string;
 
-  ngOnInit(): void {}
+  errorText: string;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.error) {
+      this.defineErrorMsg(this.error);
+    }
+  }
+
+  private defineErrorMsg(err): void {
+    if (!err) {
+      return;
+    }
+    const fieldErr = Object.keys(err)[0];
+    this.errorText = FieldErrors[fieldErr];
+  }
 }
