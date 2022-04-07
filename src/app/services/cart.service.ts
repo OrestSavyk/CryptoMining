41356d22eef09082a6651030a0519f9edcd54a7c
Orest Savyk <orestsavyk@MacBook-Pro-von-Orest.local>
2022-04-07 17:23:23 +0300

    refactor: change url

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -7,23 +7,18 @@ import { Cart } from '../models/CartTool';
   providedIn: 'root',
 })
 export class CartService {
+  url = 'https://crypto-miningbest.herokuapp.com/minerTools';
   constructor(private httpClient: HttpClient) {}
   getTool(): Observable<Cart[]> {
-    return this.httpClient.get<Cart[]>(`http://localhost:3000/minerTools`);
+    return this.httpClient.get<Cart[]>(this.url);
   }
   removeTool(id): Observable<{}> {
-    return this.httpClient.delete(`http://localhost:3000/minerTools/${id}`);
+    return this.httpClient.delete(`${this.url}/${id}`);
   }
   addTool(newTool: Cart): Observable<Cart> {
-    return this.httpClient.post<Cart>(
-      `http://localhost:3000/minerTools`,
-      newTool
-    );
+    return this.httpClient.post<Cart>(this.url, newTool);
   }
   editTool(id: string, editedCart: Cart): Observable<Cart> {
-    return this.httpClient.put<Cart>(
-      `http://localhost:3000/minerTools/${id}`,
-      editedCart
-    );
+    return this.httpClient.put<Cart>(`${this.url}/${id}`, editedCart);
   }
 }
